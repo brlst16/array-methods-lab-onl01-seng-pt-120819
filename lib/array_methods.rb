5e2178436e83abee4afca5608409725946a96007
Brigette LeVert <bnl.levert@gmail.com>
2019-12-20 02:29:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -12,7 +12,7 @@ def using_include(array, element)
   # using_include.include?("wow")
   # using_include.include?("Tardis")
   
-  array.include?("I")
+  array.include?("element1")
   array.include?("Tardis")
   
 end
