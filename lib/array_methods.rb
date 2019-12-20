bec82d23a0e475f5d72b401b890164b39739be2a
Brigette LeVert <bnl.levert@gmail.com>
2019-12-20 01:35:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def using_include(array, element)
-  ["wow", "I", "am", "really", "learning", "arrays!"]
-  #using_include.include?("wow")
-  #using_include.include?("Tardis")
+ # ["wow", "I", "am", "really", "learning", "arrays!"]
+  using_include.include?("wow")
+  using_include.include?("Tardis")
 end
 
 def using_sort(array)
