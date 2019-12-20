2062a10adcf37c0cba2aadc07f0b9e2df34c0116
Brigette LeVert <bnl.levert@gmail.com>
2019-12-20 01:23:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,4 +1,4 @@
-def using_include(array, element) =
+def using_include(array, element)
   ["wow", "I", "am", "really", "learning", "arrays!"]
   using_include.include?("wow")
   using_include.include?("Tardis")
